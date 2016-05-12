4e4554877b717d9497791ef37fcf04e4c19244d5
napestershine <napestershine@gmail.com>
2016-05-12 17:22:14 +0530

    added registration event listener

--- src/Usr/DashboardBundle/Controller/DefaultController.php
+++ src/Usr/DashboardBundle/Controller/DefaultController.php
@@ -4,6 +4,8 @@ namespace Usr\DashboardBundle\Controller;
 
 use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
@@ -16,8 +18,15 @@ class DefaultController extends Controller
             throw new AccessDeniedException('This user does not have access to this section.');
         }
 
-        return $this->render('UsrDashboardBundle:Default:index.html.twig', array(
-            'user' => $user
+        
+
+        $form = $this->createFormBuilder()
+            ->add('task', ChoiceType::class)
+            ->add('save', SubmitType::class, array('label' => 'Create Task'))
+            ->getForm();
+
+        return $this->render('@UsrDashboard/Default/index.html.twig', array(
+            'form' => $form->createView(),
         ));
     }
 
