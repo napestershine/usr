c16ded4eba1223f8e96cb6b3684b38100e8390c1
napestershine <napestershine@gmail.com>
2016-05-12 15:18:20 +0530

    fixed target entity

--- src/Usr/UserBundle/Entity/User.php
+++ src/Usr/UserBundle/Entity/User.php
@@ -19,7 +19,7 @@ class User extends BaseUser
     protected $id;
 
     /**
-     * @ORM\ManyToMany(targetEntity="MyProject\MyBundle\Entity\Group")
+     * @ORM\ManyToMany(targetEntity="Usr\UserBundle\Entity\Group")
      * @ORM\JoinTable(name="fos_user_user_group",
      *      joinColumns={@ORM\JoinColumn(name="user_id", referencedColumnName="id")},
      *      inverseJoinColumns={@ORM\JoinColumn(name="group_id", referencedColumnName="id")}
