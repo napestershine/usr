ac724b9c6c6bb6afb10233f771a44fe729bf218e
napestershine <napestershine@gmail.com>
2016-05-12 18:10:40 +0530

    created form for group choice

--- src/Usr/UserBundle/Form/GroupchoiceType.php
+++ src/Usr/UserBundle/Form/GroupchoiceType.php
@@ -1,7 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Kri
- * Date: 12-05-2016
- * Time: 18:06
- */
+
+namespace Usr\AdminBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GroupchoiceType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('group', ChoiceType::class)
+            ->add('Submit', SubmitType::class);
+    }
+
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Usr\AdminBundle\Entity\Post'
+        ));
+    }
+}
