f3fa90f18a32e99aff2ea96aac6915dbce21a2e3
napestershine <napestershine@gmail.com>
2016-05-12 16:47:13 +0530

    added dashboard page

--- src/Usr/DashboardBundle/Controller/DefaultController.php
+++ src/Usr/DashboardBundle/Controller/DefaultController.php
@@ -2,12 +2,27 @@
 
 namespace Usr\DashboardBundle\Controller;
 
+use FOS\UserBundle\Model\UserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class DefaultController extends Controller
 {
-    public function indexAction()
+    public function indexAction(Request $request)
     {
-        return $this->render('UsrDashboardBundle:Default:index.html.twig');
+        $user = $this->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        return $this->render('UsrDashboardBundle:Default:index.html.twig', array(
+            'user' => $user
+        ));
+    }
+
+    public function contactprofileAction()
+    {
+        return $this->render('@UsrDashboard/Default/contact-profile.html.twig');
     }
 }
