46d49e0ebae2bb2987864c08d5ed19cb5c18b72f
napestershine <napestershine@gmail.com>
2016-05-26 10:05:19 +0530

    added more properties to userclass

--- src/Usr/UserBundle/Entity/User.php
+++ src/Usr/UserBundle/Entity/User.php
@@ -18,9 +18,255 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @ORM\Column(name="first_name", type="text", nullable=true)
+     */
+    protected $firstName;
+
+    /**
+     * @ORM\Column(name="last_name", type="text", nullable=true)
+     */
+    protected $lastName;
+
+    /**
+     * @ORM\Column(name="address_1", type="string", length=100, nullable=true)
+     */
+    protected $streetAddress;
+
+    /**
+     * @ORM\Column(name="address_2", type="string", length=100, nullable=true)
+     */
+    protected $address2;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     */
+    protected $city;
+
+    /**
+     * @ORM\Column(type="string", length=20, nullable=true)
+     */
+    protected $state;
+
+    /**
+     * @ORM\Column(type="string", length=10, nullable=true)
+     */
+    protected $zipCode;
+
+    /**
+     * @ORM\Column(type="string", length=40, nullable=true)
+     */
+    protected $country;
+
+    /**
+     * @ORM\Column(type="string", length=14, nullable=true)
+     */
+    protected $phone;
+
+    /**
+     * @ORM\Column(name="linkedin", type="string", length=512, nullable=true)
+     */
+    protected $linkedin;
+
+    /**
+     * @ORM\Column(type="profil_image", type="string", length=255, nullable=true)
+     */
+    protected $profilImage;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * @param mixed $firstName
+     *
+     * @return $this
+     */
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param mixed $lastName
+     *
+     * @return $this
+     */
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param mixed $streetAddress
+     *
+     * @return $this
+     */
+    public function setStreetAddress($streetAddress)
+    {
+        $this->streetAddress = $streetAddress;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStreetAddress()
+    {
+        return $this->streetAddress;
+    }
+
+    /**
+     * @param mixed $address2
+     *
+     * @return $this
+     */
+    public function setAddress2($address2)
+    {
+        $this->address2 = $address2;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAddress2()
+    {
+        return $this->address2;
+    }
+
+    /**
+     * @param mixed $city
+     *
+     * @return $this
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param mixed $state
+     *
+     * @return $this
+     */
+    public function setState($state)
+    {
+        $this->state = $state;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getState()
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param mixed $zipCode
+     *
+     * @return $this
+     */
+    public function setZipCode($zipCode)
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getZipCode()
+    {
+        return $this->zipCode;
+    }
+
+    /**
+     * @param mixed $country
+     *
+     * @return $this
+     */
+    public function setCountry($country)
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+    /**
+     * @param mixed $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+    /**
+     * @return mixed
+     */
+    public function getLinkedin()
+    {
+        return $this->linkedin;
+    }
+
+    /**
+     * @param mixed $linkedin
+     */
+    public function setLinkedin($linkedin)
+    {
+        $this->linkedin = $linkedin;
     }
 }
